c5aba373184a75e3145a006c3f51c9dd0960153b
nithinsai060905-ui <nithinsai060905@gmail.com>
2025-12-18 12:23:02 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,23 @@
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 11;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n ||
+                    j == 1 || j == n ||
+                    i == (n / 2) + 1 ||
+                    j == (n / 2) + 1) {
+
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
